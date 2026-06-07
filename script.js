9313fb583b16647fb2f3477a01b9a256108dedf1
bensonming5-cyber <bensonming5@gmail.com>
2026-06-07 08:37:39 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const header = document.querySelector("[data-header]");
+
+if (header) {
+  const updateHeader = () => header.classList.toggle("is-scrolled", window.scrollY > 24);
+  updateHeader();
+  window.addEventListener("scroll", updateHeader, { passive: true });
+}
